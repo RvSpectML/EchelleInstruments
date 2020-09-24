622ae6f89d038047928cdd96220624ded43a1a47
Eric Ford <eford@psu.edu>
2020-09-24 12:08:54 -0400

    retry

---
+++ src/tellurics.jl
@@ -0,0 +1,7 @@
+"""
+   find_worst_telluric_in_each_chunk( chunk_list_timeseries, array_of_spectra )
+Returns a vector of the worst telluric (i.e., smallest value) within each chunk at any observations.
+
+Warning: Assumes a tellurics value in metadata for each spectra, such as is provided by EXPRES.
+"""
+function find_worst_telluric_in_each_chunk end
