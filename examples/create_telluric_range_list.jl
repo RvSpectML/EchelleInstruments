c1a28385edf6b3d67242ba769614a8966e7938a6
Eric Ford <eford@psu.edu>
2020-10-14 01:57:51 -0400

    create telluric ranges combines all obs, not juse first

--- examples/create_telluric_range_list.jl
+++ examples/create_telluric_range_list.jl
@@ -4,18 +4,22 @@ include("read_expres_data_101501.jl")
 #df_for_one_spectra = EXPRES.find_ranges_with_tellurics(all_spectra[5])
 
 # Overwrite barycentric wavelengths with observatory-frame wavelengths
-map(obsid->all_spectra[1].λ .= FITSIO.read(FITS(df_files_use.Filename[1])["optimal"],"wavelength"), 1:length(all_spectra) )
+using FITSIO
+map(obsid->all_spectra[obsid].λ .= FITSIO.read(FITS(df_files_use.Filename[obsid])["optimal"],"wavelength"), 1:length(all_spectra) )
 
 wavelength_ranges_with_tellurics = EchelleInstruments.find_ranges_with_tellurics(all_spectra, min_Δv_clean=EXPRES.default_min_Δv_clean)
 
-using CSV
-CSV.write(joinpath(output_dir,"telluric_ranges.csv"),wavelength_ranges_with_tellurics)
+#using CSV
+#CSV.write(joinpath(output_dir,"telluric_ranges.csv"),wavelength_ranges_with_tellurics)
 
 min_λ_min = minimum(map(i->minimum(all_spectra[i].λ[.!isnan.(all_spectra[i].metadata[:tellurics]) .& (all_spectra[i].metadata[:tellurics] .== 1) ]), 1:length(all_spectra) ))
 max_λ_max = maximum(map(i->maximum(all_spectra[i].λ[.!isnan.(all_spectra[i].metadata[:tellurics]) .& (all_spectra[i].metadata[:tellurics] .== 1) ]), 1:length(all_spectra) ))
 
 wavelength_ranges_without_tellurics = EchelleInstruments.make_ranges_without_tellurics(wavelength_ranges_with_tellurics,
       λ_start = min_λ_min, λ_stop = max_λ_max, min_Δv= 1000)
+
+wavelength_ranges_without_tellurics2 = EchelleInstruments.make_ranges_without_tellurics(wavelength_ranges_with_tellurics,
+            λ_start = min_λ_min, λ_stop = max_λ_max, min_Δv= 1000, max_Δv = 1000000)
 
 #CSV.write(joinpath(output_dir,"telluric_free_ranges_min_Δv=1000.csv"),wavelength_ranges_without_tellurics)
 
